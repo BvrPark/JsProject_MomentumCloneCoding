f7195baac2546903c20e6dd1944889e7eaf58875
crupy <wlxh95@naver.com>
2021-08-23 23:33:30 +0900

    const와 let의 차이점

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const a = 10;
 const b = 3;
-const myName = "nico";
+let myName = "nico";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "nicolas";
+console.log("your new name is" + myName);
