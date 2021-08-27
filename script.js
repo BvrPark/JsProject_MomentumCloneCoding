26e2f8c49c2665a034259337619b3cec2a5749ce
crupy <wlxh95@naver.com>
2021-08-27 23:23:59 +0900

    array의 개념과 push의 쓰임새

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const amIFat = null;
-let something;
-console.log(something, amIFat);
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat];
+
+//Get Item from Array
+console.log(daysOfWeek[4]);
+
+//Add one more dat to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
