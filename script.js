084e5e27e7259b1f11c4a860c7af85b60d9d6a78
crupy <wlxh95@naver.com>
2021-08-28 15:21:38 +0900

    object에 대한 설명

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
-const fri = "fri";
-const sat = "sat";
-const sun = "sun";
-
-const daysOfWeek = [mon, tue, wed, thu, fri, sat];
-
-//Get Item from Array
-console.log(daysOfWeek[4]);
-
-//Add one more dat to the array
-daysOfWeek.push("sun");
-
-console.log(daysOfWeek);
+//object에 대한 설명
+const player = {
+    name: "nico",
+    points: 10,
+    fat: true,
+};
+console.log(player);
+//object 수정
+player.fat = false;
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+//object에 새로운 것 추가
+player.lastName = "potato";
