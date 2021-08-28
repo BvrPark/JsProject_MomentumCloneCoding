ed64a192e87a5c682a7a7ebcbde3239d1fab3e11
crupy <wlxh95@naver.com>
2021-08-28 15:49:05 +0900

    function과 object를 같이 쓰기

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-//function
-function sayHello(nameOfPerson,age){
-    console.log("Hello my name is " + nameOfPerson
-                +"and I'm " + age);
-}
-sayHello("nico", 10);
-sayHello("dal", 23);
-sayHello("lynn", 21);
+const player = {
+    name: "nico",
+    sayHello: function(otherPersonsName) {
+        console.log("hello! " + otherPersonsName + " nice to meet you");
+    },
+};
+
+console.log(player.name);
+player.sayHello("lynn");
+player.sayHello("nico");
