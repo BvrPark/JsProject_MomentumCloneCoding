fdb169e1d6b05504f15d4d10b11122a631b2b9fe
crupy <wlxh95@naver.com>
2021-08-28 15:37:31 +0900

    function의 사용법

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-//object에 대한 설명
-const player = {
-    name: "nico",
-    points: 10,
-    fat: true,
-};
-console.log(player);
-//object 수정
-player.fat = false;
-console.log(player);
-console.log(player.name);
-console.log(player["name"]);
-//object에 새로운 것 추가
-player.lastName = "potato";
+//function
+function sayHello(nameOfPerson,age){
+    console.log("Hello my name is " + nameOfPerson
+                +"and I'm " + age);
+}
+sayHello("nico", 10);
+sayHello("dal", 23);
+sayHello("lynn", 21);
