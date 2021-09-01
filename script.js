a59b6410d7d71a2f4aeaa91e324158d24beca4d7
crupy <wlxh95@naver.com>
2021-09-01 16:59:29 +0900

    console.log가 아닌 return형으로 계산기 다시 고침

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const calculator = {
     add: function(a, b){
-        console.log(a+b);
+        return a+b;
     },
     min: function(a, b){
-        console.log(a-b);
+        return a-b;
     },
     mul: function(a, b){
-        console.log(a*b);
+        return a*b;
     },
     div: function(a, b){
-        console.log(a/b);
-    },
+        return a/b;
+    }
 };
+
+const plusResult = calculator.add(2, 3);
+
+console.log(plusResult);
