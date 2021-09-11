33aa426e29f77d3b780a9bb01ce7b5ef0e1abf8d
crupy <wlxh95@naver.com>
2021-09-11 18:58:33 +0900

    다른 event들 예시

--- script.js
+++ script.js
@@ -3,5 +3,23 @@ const title = document.querySelector("div.hello:first-child h1");
 function handleTitleClick(){
     console.log("title was clicked!");
 }
-
+title.onclick = handleTitleClick;
 title.addEventListener("click", handleTitleClick);
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy(){
+    alert("copier!");
+
+}
+function handleWindowOffline(){
+    alert("SOS no WIFI");
+}
+function handleWindowOnline(){
+    alert("ALL GOOOOOD");
+}
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
