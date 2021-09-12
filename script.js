d169481529ed5546aafee28a5b3cc8e744a89ec4
crupy <wlxh95@naver.com>
2021-09-12 22:55:55 +0900

    자잘한 기능들을 빼고 if를 이용해서 javascript로 css구현

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
 const title = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    console.log("title was clicked!");
+    const currentColor = title.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor = "tomato";
+    }else {
+        newColor = "blue";
+    }
+    title.style.color = newColor;
 }
-title.onclick = handleTitleClick;
 title.addEventListener("click", handleTitleClick);
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy(){
-    alert("copier!");
-
-}
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-function handleWindowOnline(){
-    alert("ALL GOOOOOD");
-}
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
