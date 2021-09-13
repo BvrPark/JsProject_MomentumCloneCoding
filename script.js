5f0c73a95122fd190ba48d72747279844914dc0d
crupy <wlxh95@naver.com>
2021-09-13 17:13:04 +0900

    toggle의 기능에 대한 설명
    classList에 특정한 클래스가 포함되어 있으면
    클래스 이름만 제거를 하고 포함되어 있지않으면 추가를 해주는 기능

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    const currentColor = title.style.color;
-    let newColor;
-    if(currentColor === "blue"){
-        newColor = "tomato";
-    }else {
-        newColor = "blue";
-    }
-    title.style.color = newColor;
+    h1.classList.toggle("active");
 }
-title.addEventListener("click", handleTitleClick);
+h1.addEventListener("click", handleTitleClick);
