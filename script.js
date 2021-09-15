0eafb3ff1d22c7f60618953010a1e55c7415a36a
crupy <wlxh95@naver.com>
2021-09-15 13:56:51 +0900

    많이 수정함
    1. form이 있는 이상 button의 event가 필요가 없으므로 loginButton변수를 없애고 대신 loginForm이라는 변수를 추가
    2. onLoginSubmit이라는 함수에서 <form>에서 페이지가 새로고침되는 것을 막고 hidden이라는 클래스를 추가해준 뒤 localStorage로 입력한 username을 저장한다.
    3. greeting이라는 <h1>의 아이디 값에 innerText로 내용을 추가해주고 classList.remove로 hidden이라는 클래스를 제거해준다.
    4. greeting.innerText와 greeting.classList.remove가 반복이 되므로 묶어서 paintGreetings라는 하나의 function으로 묶어준다.
    5. if문에서 savedUsername에 저장된 아이디 값이 없으면 form에서 hidden클래스를 제거하고 submit이 발생할 시, onLoginSubmit 함수를 실행시키고
    6. if문에서 savedUsername에 저장된 아이디 값이 있으면 paintGreetings함수를 실행시킨다.

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function loginClick(){
-    console.log(loginInput.value);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    // 어떤 이벤트의 기본행동이든지 실행되지 않게 막음
+    // 여기에선 form에서 클릭이나 엔터를 입력할시 페이지가 새로고침되는 것을 막음
+    
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    
+    localStorage.setItem(USERNAME_KEY, username);
+    //데이터베이스와 같은 기능
+    //""안은 key값 ,뒤에는 value값
+    
+    greeting.innerText = `Hello ${username}`;
+    //string과 변수를 합치는 법
+    //'가 아닌 `다 헷갈리지 않도록!!!
+
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
 }
 
-loginButton.addEventListener("click", loginClick);
+function paintGreetrings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else{
+    //show the greetings
+    paintGreetrings(savedUsername);
+   
+}
