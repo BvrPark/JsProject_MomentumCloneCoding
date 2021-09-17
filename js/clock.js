83beb959bb6b5397b5c24260319b68b1b568cd6f
crupy <wlxh95@naver.com>
2021-09-17 11:56:06 +0900

    getClock()이라는 함수를 만듬
    1. getClock()과 setInterval이라는 함수를 이용해서 실시간으로 시계가 계속 바뀌게 만듬
    2. hours, minutes, seconds의 변수에 각각 실시간으로 바뀌는 date.get~을 추가
    3. padStart()라는 함수는 String형태에만 적용이 되므로 hours, minutes, seconds는 숫자로 입력받으므로 String으로 형변환시켜서 padStart 함수를 적용시킴

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,21 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello(){
-    console.log("hello");
+function getClock(){
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    // padStart(a,b)
+    // a : string형으로 지정할 길이
+    // b : string형의 길이가 a만큼이 되지않으면 string형 앞에 b를 추가
+    // padEnd(a,b)
+    // 다른것은 padStart와 똑같고 b를 뒤에 추가
+    
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-setInterval(sayHello, 5000);
+getClock();
+setInterval(getClock, 1000);
 // setInterval(호출할 함수이름, 호출할 간격(ms))
+
+
