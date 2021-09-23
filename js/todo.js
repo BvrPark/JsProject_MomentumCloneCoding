e907d06ed9b4c0ae864c3bc1cf3375274dae1e37
crupy <wlxh95@naver.com>
2021-09-23 22:13:30 +0900

    toDoList에 내용을 추가하는 function들 생성
    li와 span을 생성하는 function인 paintToDo생성
    6번째 줄에서 li태그를 html에 생성, 7번째 줄에서 span태그를 html에 생성
    8번째 줄에서 toDoList에 li태그를 생성
    9번째 줄에서 appendChild()를 이용해 li태그 안에 span태그를 생성
    10번째 줄에서 span태그안에 newToDo에서 입력되어 있던 Text를 나오게 해준다.
    17번째 줄에 paintToDo function을 추가

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    toDoList.appendChild(li);   //toDolist에 li태그 추가
+    li.appendChild(span);       //li태그안에 span태그 추가
+    span.innerText = newToDo;
+}
 
 function handleToDoSubmit(event){
-    event.preventDefault();
+    event.preventDefault();     //submit하고 새로고침 중지
     const newToDo = toDoInput.vaule;
     toDoInput.vaule = "";
+    paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
