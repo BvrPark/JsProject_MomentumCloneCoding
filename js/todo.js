e122caa708e35f4d830f57c2e746989fe9a3a2b6
crupy <wlxh95@naver.com>
2021-09-24 23:55:53 +0900

    오타 수정 및 delete 기능 추가
    25,26번째 줄에 value를 vaule로 잘못쓴것을 수정함
    5번째 줄에 delete기능을 하는 함수를 만듬
    6번째 줄에 li라고 하는 변수에 이 delete function이 가르키는 event의 target을 설정하고 거기의 parentElement를 찾음
    7번째 줄에 그 element를 지우는 remove()를 씀
    14번째 줄에 버튼기능을 추가
    16번째 줄에 버튼을 클릭시 deleteToDo함수 실행하게 만듬

--- js/todo.js
+++ js/todo.js
@@ -2,18 +2,28 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newToDo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    toDoList.appendChild(li);   //toDolist에 li태그 추가
-    li.appendChild(span);       //li태그안에 span태그 추가
     span.innerText = newToDo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);       //li태그안에 span태그 추가
+    li.appendChild(button);     //button을 li의 마지막에 추가
+    toDoList.appendChild(li);   //toDolist에 li태그 추가
+    
 }
 
 function handleToDoSubmit(event){
     event.preventDefault();     //submit하고 새로고침 중지
-    const newToDo = toDoInput.vaule;
-    toDoInput.vaule = "";
+    const newToDo = toDoInput.value;
+    toDoInput.value = "";
     paintToDo(newToDo);
 }
 
