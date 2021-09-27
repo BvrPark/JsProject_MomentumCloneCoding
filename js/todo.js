6328a1ca79478d7b654525143ae94ffb0391a68a
crupy <wlxh95@naver.com>
2021-09-27 10:54:15 +0900

    버튼을 누를시 localStorage에서도 삭제하는 방법 - 2
    18번째 줄에서 toDos.filter로 삭제할 조건을 입력한 뒤 실행하면 되는데 여기서 toDo.id는 number이고 li.id는 String이므로 li.id를 형변환시켜줘야한다.
    그리고 19번째 줄에 saveToDo()를 다시 불러와줘야 array가 정상적으로 작동된다.

--- js/todo.js
+++ js/todo.js
@@ -15,6 +15,8 @@ function saveToDo(){
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+    saveToDo();
 }
 
 function paintToDo(newToDo){
