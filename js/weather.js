d06b640e66ff19b6fe02e2df9785866a13aa2007
crupy <wlxh95@naver.com>
2021-09-27 12:10:28 +0900

    weather.js 완성
    우선 openweathermap.org 사이트의 API를 이용해서 날짜 정보를 받는다.
    7번째 줄에 fetch는 url을 바로 받아서 실행하는게 아니고 한박자 쉰다음에 실행을 해서 위에 then으로 연결시켜준다.

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,19 @@
-function onGeoOk(){
+const API_KEY = "86ea555258d2f8a1eb76d6dd8061a78f";
+
+function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json())
+    .then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError(){
     alert("Can't find you.")    
 }
-navigator.geolocation.getCurrentPosition(onGeoOk, OnGeoError);
-//
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+//사용자의 현재 위치를 나타내주는 함수
